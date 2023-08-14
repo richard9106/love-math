3838f1f69096ceb16e634a105987905d9346a859
Richard Marles <marleswebinfo@gmail.com>
2023-08-14 14:50:43 +0200

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function(){
           alert("Yous cliked  submit!")
         }else{
           let gameType  = this.getAttribute("data-type");
-          alert(`You clikc ${gameType} button`); 
+          runGame(gameType); 
         }
       })
   }
+
+  runGame("addition");
 })
 
 /**
@@ -21,10 +23,17 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's anwer has been processed 
  */
 
-function runGame(){
+function runGame(gameType){
   //creats random numbers between 1 and 25
   let num1 = Math.floor(Math.random()*25)+1;
   let num2 = Math.floor(Math.random()*25)+1;
+
+  if(gameType === "addition"){
+    displayAdditionQuestion(num1,num2);
+  }else{
+    alert(`Unknown game type ${gameType}`);
+    throw `Unknown game type ${gameType} Aborting!`;
+  }
 }
 function checkAnswer(){
 
@@ -42,8 +51,13 @@ function incrementWrongAnswer(){
   
 }
 
-function displayAdditionQuestion(){
-  
+function displayAdditionQuestion(operand1,operand2){
+
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="+";
+
+
 }
 
 function displaySbutractQuestion(){
