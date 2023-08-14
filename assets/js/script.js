40e63aab5af4c6e29187419885e7d9b451293942
Richard Marles <marleswebinfo@gmail.com>
2023-08-14 13:54:26 +0200

    Set up sturcture of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+  
+}
+
+function incrementWrongAnswer(){
+  
+}
+
+function displayAdditionQuestion(){
+  
+}
+
+function displaySbutractQuestion(){
+  
+}
+
+function displayMultiplyQuestion(){
+  
+}
+
+function displayDivisionQuestion(){
+  
+}
