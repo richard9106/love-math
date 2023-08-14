663cbf05d658b82fded315c81ab876aa1efacb51
Richard Marles <marleswebinfo@gmail.com>
2023-08-14 14:10:53 +0200

    Adding event listener for loanding document and buttons clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//Wait for the DOM to finish loanding before runing the game.
+//Get the buttons elements and addEventListener to them. 
+
+document.addEventListener("DOMContentLoaded", function(){
+  let buttons=document.getElementsByTagName("button");
+
+  for(let button of buttons){
+      button.addEventListener("click", function(){
+        if(this.getAttribute("data-type")=="submit"){
+          alert("Yous cliked  submit!")
+        }else{
+          let gameType  = this.getAttribute("data-type");
+          alert(`You clikc ${gameType} button`); 
+        }
+      })
+  }
+})
+
 function runGame(){
 
 }
